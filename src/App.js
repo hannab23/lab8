afc142f72692a8c4b8a5d7c6467297e4b155f532
hannab23 <hboguta23567@gmail.com>
2021-12-05 20:49:36 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState("");
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInput(values => ({...values, [name]: value}))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Fibonacci</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Ktory element ciagu Fibonacciego chcesz wyswietlic?
+        </p>
+          <input 
+            type="number" 
+            name="element"
+            min="0"
+            size="5"
+            font-size="30px"
+            defaultValue="0"
+            onChange={handleChange}
+            />
+        <p>
+          Element nr {input.element} ma wartosc: {fibonacci(input.element)}
+        </p>
       </header>
     </div>
   );
+}
+
+function fibonacci(element) {
+  var x = 1;
+  var y = 0;
+  var temp;
+  while(element > 0) {
+    temp = x;
+    x = x + y;
+    y = temp;
+    element--;
+  }
+  return y;
 }
 
 export default App;
